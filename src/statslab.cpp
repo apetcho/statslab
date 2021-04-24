5d6b593bdd4114a1b4794c621b612315991fab9a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-04-24 17:15:35 +0200

    Implement CenterInfo::harmonic_mean()

--- src/statslab.cpp
+++ src/statslab.cpp
@@ -1,6 +1,7 @@
 #include "statslab.hpp"
 #include<iostream>
 #include<cstdlib>
+#include<algorithm>
 
 namespace statslab{
 
@@ -54,5 +55,75 @@ double CenterInfo::geometric_mean(){
 }
 //
 
+/**
+ * @brief CenterInfo::harmonic_mean()
+ * Compute the harmonic mean of data.
+ * 
+ * The harmonic mean is the reciprocal of the arithmetic mean of the 
+ * reciprocals of the data. It can be used for averarging ratios or 
+ * rates, for example speeds.
+ * 
+ * @param weight 
+ * @return double 
+ */
+double CenterInfo::harmonic_mean(std::vector<double> weights){
+    //
+    auto errmsg = "harmonic mean does not support negative values"
+    auto N = data_.size();
+    if(n < 1){
+        std::cerr << "harmonic_mean() requires at least one data point"
+            << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    else if(N == 1 && weights.empty()){
+        auto x = data_[0];
+        if(x < 0){
+            std::cerr << errmsg << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+        return x;
+    }
+
+    double sumWeights = 0.0;
+    if(weights.empty()){
+        for(size_t i=0; i < N; ++i){
+            weights.push_back(1.0);
+        }
+        sumWeights = (double)(N);
+    }
+    else{
+        if(weights.size() != N){
+            std::cerr << "Number of weight does not much data size"
+                << std::endl;
+        }
+        //
+        for(auto elt: weights){
+            if(elt < 0.0){
+                std::cerr << errmsg << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
+            sumWeights += elt;
+        }
+    }
+    //
+    double total = 0.0;
+    if(data_.min() < 0.0){
+        std::cerr << errmsg << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    try{
+        for(size_t i=0; i < N; i++{
+            auto w = weights[i];
+            auto v = data_[i];
+            total += w/x;
+        }
+    }
+    catch(...){ // ZeroDivisionError
+        return 0.0;
+    }
+
+    auto val = sumWeights / total;
+    return val;
+}
 //
 }
